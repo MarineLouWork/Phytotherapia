8ffa3da57a48b500dcfd4e290de625e2739c807b
MarineLouWork <marinelou.work@gmail.com>
2023-01-20 14:23:27 +0100

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let burger = document.querySelector(".burger")
+let menuVisible = false;
+burger.addEventListener('click', () => {
+    if (menuVisible == false) {
+        menuVisible = true
+        document.querySelector("nav").style.display = "block"
+    } else {
+        menuVisible = false
+        document.querySelector("nav").style.display = "none"
+    }
+})
